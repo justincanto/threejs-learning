ce220a9b9d99b0a0aa264e7fdb6fdba3d59da197
Justin Canto <justinlcanto@gmail.com>
2022-09-18 18:51:13 +0200

    basic physics

--- 22-physics/src/script.js
+++ 22-physics/src/script.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import CANNON from 'cannon'
 
 /**
  * Debug
@@ -31,6 +32,40 @@ const environmentMapTexture = cubeTextureLoader.load([
     '/textures/environmentMaps/0/pz.png',
     '/textures/environmentMaps/0/nz.png'
 ])
+
+const world = new CANNON.World();
+world.gravity.set(0, -9.82, 0);
+
+const defaultMaterial = new CANNON.Material('default');
+
+const defaultContactMaterial = new CANNON.ContactMaterial(
+    defaultMaterial,
+    defaultMaterial,
+    {
+        friction: 0.1,
+        restitution: 0.7
+    }
+)
+world.addContactMaterial(defaultContactMaterial);
+world.defaultContactMaterial = defaultContactMaterial;
+
+const sphereShape = new CANNON.Sphere(0.5);
+const sphereBody = new CANNON.Body({
+    mass: 1, 
+    position: new CANNON.Vec3(0, 3, 0),
+    shape: sphereShape,
+})
+
+world.addBody(sphereBody);
+
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body({
+    mass: 0,
+    shape: floorShape,
+});
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
+
+world.addBody(floorBody);
 
 /**
  * Test sphere
@@ -132,10 +167,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let oldElapsedTime = 0;
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - oldElapsedTime;
+    oldElapsedTime = elapsedTime;
+
+    world.step( 1/60, deltaTime, 3);
+
+    sphere.position.copy(sphereBody.position);
 
     // Update controls
     controls.update()
